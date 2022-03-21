adef20c419e36305d496120d20a57a341249c7d0
One <tradgardsmastaren@protonmail.com>
2022-03-21 13:56:49 +0100

    setting up project

---
+++ src/main/java/com/onetrillion/cactustore/CactustoreApplication.java
@@ -0,0 +1,13 @@
+package com.onetrillion.cactustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactustoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CactustoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onetrillion/cactustore/CactustoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetrillion.cactustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactustoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
